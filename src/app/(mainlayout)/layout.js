84442fc55d21019d2c5f8206fff42805a5f215b2
elhazen98 <elhazen98@gmail.com>
2025-03-03 23:35:28 +0700

    chore: delete comment at (mainlayout)/layout.js

--- src/app/(mainlayout)/layout.js
+++ src/app/(mainlayout)/layout.js
@@ -4,13 +4,6 @@ import { Footer } from "./_components/footer";
 import { Header } from "./_components/header";
 
 export default async function Layout({ children }) {
-    // const session = await auth();
-    // if (!session) {
-    //     redirect("/login");
-    // }
-
-    // const results = await prisma.results.findMany();
-
     return (
         <div className="h-screen border-1 border-red-700 flex flex-col justify-between">
             <Header />
