c6f0fe71981a36cd3e2a9c6f15687a7b71c44540
elhazen98 <elhazen98@gmail.com>
2025-03-03 20:55:18 +0700

    feat: adding prisma schema

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return (
-    <div>
-      <Button className="bg-blue-600 text-white hover:bg-blue-700">
-        Click me
-      </Button>
-    </div>
-  );
+    return (
+        <div>
+            <Button className="bg-blue-600 text-white hover:bg-blue-700">
+                Click me
+            </Button>
+            <Slider defaultValue={[33]} max={100} step={1} />
+            <Input />
+        </div>
+    );
 }
