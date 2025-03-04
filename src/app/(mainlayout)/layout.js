7159d7260d3c9a1e4152e6bfee1eb04bb763173b
elhazen98 <elhazen98@gmail.com>
2025-03-04 23:03:10 +0700

    feat: create history result side bar

--- src/app/(mainlayout)/layout.js
+++ src/app/(mainlayout)/layout.js
@@ -7,7 +7,7 @@ export default async function Layout({ children }) {
         <div className="h-screen border-1 border-red-700 flex flex-col justify-between">
             <Header />
             <div className="flex h-full">
-                <Sidebar />
+                <Sidebar userId={"d9479705-779c-43ec-9646-ea379c511932"} />
                 <div className="w-full flex items-center justify-center border-1 border-red-700">
                     {children}
                 </div>
