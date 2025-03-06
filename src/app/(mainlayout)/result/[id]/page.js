aa9697220636780a9af91a3267fb12297ebe7fd4
akbarulazis <73051141+akbarulazis@users.noreply.github.com>
2025-03-07 00:26:57 +0700

    feat: add ai integration open ai to get response

--- src/app/(mainlayout)/result/[id]/page.js
+++ src/app/(mainlayout)/result/[id]/page.js
@@ -1,4 +1,8 @@
+import { aiAction } from "./action";
+
 export default async function Page({ params }) {
-    const { id } = await params;
-    return <div>{id}</div>;
+  const { id } = await params;
+  const response = await aiAction();
+  console.log(response);
+  return <div>{id}</div>;
 }
