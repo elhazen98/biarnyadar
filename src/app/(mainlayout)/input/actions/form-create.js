6953f0ce87a9eb099e5f7dd76bf53a4fcf3005d7
AsterixD2 <asterixd2@yahoo.com>
2025-03-10 23:21:32 +0700

    finish setup input

--- src/app/(mainlayout)/input/actions/form-create.js
+++ src/app/(mainlayout)/input/actions/form-create.js
@@ -1,7 +1,8 @@
 "use server";
-// import { API_URL } from "@/constants/api-url";
+
 import { revalidatePath } from "next/cache";
 import { Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -9,17 +10,39 @@ export async function dataInput(_, formData) {
   //simulate delay
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  const name = formData.get("name");
   const age = formData.get("age");
   const height = formData.get("height");
   const weight = formData.get("weight");
+  const bmi = formData.get("bmi");
+  const diet = formData.get("diet");
+  const workout = formData.get("workout");
+  const smoking = formData.get("smoking");
+  const alcohol = formData.get("alcohol");
+  const sleepTime = formData.get("sleepTime");
+  const stress = formData.get("stress");
+  const location = formData.get("location");
+  const roastLevel = formData.get("roastLevel");
 
-  if (!name || !age || !height || !weight) {
+  if (!age || !height || !weight) {
     return { status: "error", message: "All fields are required" };
   }
 
   await prisma.todo.create({
-    data: { name, age, height, weight },
+    data: {
+      name,
+      age,
+      height,
+      weight,
+      bmi,
+      diet,
+      workout,
+      smoking,
+      alcohol,
+      sleepTime,
+      stress,
+      location,
+      roastLevel,
+    },
   });
 
   revalidatePath("/");
