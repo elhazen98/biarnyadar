5c5d90c75ca58327ba74774dcc2343995e8d5508
elhazen98 <elhazen98@gmail.com>
2025-03-12 23:41:17 +0700

    feat: add 'new input' button on sidebar

--- src/app/(mainlayout)/_components/sidebar.js
+++ src/app/(mainlayout)/_components/sidebar.js
@@ -9,19 +9,30 @@ export const Sidebar = async ({ userId }) => {
         <div className="max-w-md w-1/6 pl-4 pr-4">
             <div className="h-full border border-slate-700/50 rounded-xl from-gray-800/20 to-gray-700/20 backdrop-blur-md shadow-lg bg-gradient-to-b">
                 <div className="p-4 h-full space-y-4">
-                    <div className="font-bold text-xl">History</div>
-                    <div className="flex flex-col gap-2">
-                        {results.map((result) => (
-                            <Link
-                                className=""
-                                href={`/result/${result.inputId}`}
-                                key={result.inputId}
-                            >
-                                <div className="w-full text-sm rounded-lg transition duration-150 text-slate-300 hover:bg-purple-300 hover:text-slate-900 hover:shadow-lg p-2 bg-slate-700/50 font-bold">
-                                    {result.title}
-                                </div>
-                            </Link>
-                        ))}
+                    <div className="h-full flex flex-col justify-between">
+                        <div>
+                            <div className="font-bold text-xl mb-8">
+                                History
+                            </div>
+                            <div className="flex flex-col gap-2"></div>
+                            {results.map((result) => (
+                                <Link
+                                    className=""
+                                    href={`/result/${result.inputId}`}
+                                    key={result.inputId}
+                                >
+                                    <div className="w-full text-sm rounded-lg transition duration-150 text-slate-300 hover:bg-purple-300 hover:text-slate-900 hover:shadow-lg p-2 bg-slate-700/50 font-bold">
+                                        {result.title}
+                                    </div>
+                                </Link>
+                            ))}
+                        </div>
+                        <Link
+                            href={"/input"}
+                            className="text-center rounded-full text-md py-2 px-4 bg-gradient-to-r from-indigo-800 to-purple-800 hover:brightness-150 duration-150"
+                        >
+                            Add New Input
+                        </Link>
                     </div>
                 </div>
             </div>
